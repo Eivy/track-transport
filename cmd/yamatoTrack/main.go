3e8957b6f7fd1fe2339da1d753a6e4ac5cb0056a
Eivy <modern.times.rock.and.roll+git@gmail.com>
2016-12-30 17:03:05 +0900

    Add flags
    -p: pipe mode
    -i: interactive mode

--- cmd/yamatoTrack/main.go
+++ cmd/yamatoTrack/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"local/transportTrack/yamato"
@@ -10,7 +11,16 @@ import (
 )
 
 func main() {
-	if len(os.Args) < 2 {
+	var interactive bool
+	var pipe bool
+	flag.BoolVar(&interactive, "i", false, "use interactive")
+	flag.BoolVar(&pipe, "p", false, "use pipe")
+	flag.Parse()
+	if interactive && pipe {
+		fmt.Fprintln(os.Stderr, "Can not use interactive mode and pipe mode same time")
+		os.Exit(99)
+	}
+	if interactive {
 		fmt.Print("Input tracking number: ")
 		scanner := bufio.NewScanner(os.Stdin)
 		for scanner.Scan() {
@@ -26,19 +36,29 @@ func main() {
 		if err := scanner.Err(); err != nil {
 			exitWithError(err)
 		}
+	} else if pipe {
+		t, err := ioutil.ReadAll(os.Stdin)
+		if err != nil {
+			exitWithError(err)
+		}
+		if v, err := yamato.GetStatus(strings.Split(string(t), "\n")); err != nil {
+			exitWithError(err)
+		} else {
+			for _, n := range v {
+				fmt.Println(strings.Join(n, ","))
+			}
+		}
 	} else {
-		for i, v := range os.Args {
-			if i > 0 {
-				b, err := ioutil.ReadFile(v)
-				if err != nil {
-					exitWithError(err)
-				}
-				if v, err := yamato.GetStatus(strings.Split(string(b), "\n")); err != nil {
-					exitWithError(err)
-				} else {
-					for _, n := range v {
-						fmt.Println(strings.Join(n, ","))
-					}
+		for _, v := range flag.Args() {
+			b, err := ioutil.ReadFile(v)
+			if err != nil {
+				exitWithError(err)
+			}
+			if v, err := yamato.GetStatus(strings.Split(string(b), "\n")); err != nil {
+				exitWithError(err)
+			} else {
+				for _, n := range v {
+					fmt.Println(strings.Join(n, ","))
 				}
 			}
 		}
